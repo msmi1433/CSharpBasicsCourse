d5205cbe57e50d29db7b97513044d1ba63f6b588
Myles Smith <mylesbsmith96@gmail.com>
2024-10-14 16:16:58 +0100

    write example variables to the console

--- SimpleConsoleApplication/Program.cs
+++ SimpleConsoleApplication/Program.cs
@@ -1,7 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int integerExample = 3;
+string stringExample = "Goodbye, world!";
+bool booleanExample = true;
+double doubleExample = 3.3333333;
 
-int IntegerExample = 3;
-string StringExample = "Goodbye, world!";
-bool BooleanExample = true;
-double DoubleExample = 3.3333333;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Here are some examples of the basic data types in C#");
 
+Console.WriteLine($"This is an integer: '{integerExample}'");
+Console.WriteLine($"Here is a string: '{stringExample}'");
+Console.WriteLine($"Booleans can be 'False' or: '{booleanExample}'");
+Console.WriteLine($"Finally, a double: '{doubleExample}'");
