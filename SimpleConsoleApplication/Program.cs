20ce8dde74fd91031c63bb211305d4bd056143b3
Myles Smith <mylesbsmith96@gmail.com>
2024-10-23 16:17:42 +0100

    implement for and while loops

--- SimpleConsoleApplication/Program.cs
+++ SimpleConsoleApplication/Program.cs
@@ -14,3 +14,23 @@ Console.WriteLine($"Booleans can be 'False' or: '{booleanExample}'");
 Console.WriteLine($"Finally, a double: '{doubleExample}'");
 Console.WriteLine();
 Console.WriteLine($"As a bonus, here is a char: '{charExample}'");
+
+Console.WriteLine("The below is an example of the various loops available in C#");
+Console.WriteLine();
+
+Console.WriteLine("For loop:");
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+
+int whileLoopInteger = 1;
+Console.WriteLine("While loop:");
+while (whileLoopInteger <= 10)
+{
+    Console.WriteLine(whileLoopInteger);
+    whileLoopInteger += 1;
+}
+
+
