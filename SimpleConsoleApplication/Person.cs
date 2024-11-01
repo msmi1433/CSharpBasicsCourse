f9c4b2ccacc754a41772b222a5357878e5c13d56
Myles Smith <mylesbsmith96@gmail.com>
2024-11-01 15:48:05 +0000

    create Greet() method

--- SimpleConsoleApplication/Person.cs
+++ SimpleConsoleApplication/Person.cs
@@ -4,4 +4,8 @@ public class Person
 {
     private string Name;
     private int Age;
+    private void Greet()
+    {
+        Console.WriteLine($"Hello {Name}, you are {Age} years old.");
+    }
 }
